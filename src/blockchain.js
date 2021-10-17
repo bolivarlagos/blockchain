8b8db7a8e7813253dc683fe418b12f9aa9680a71
bolivarlagos <bolivarlagos@gmail.com>
2021-10-17 11:31:54 -0300

    adding block

--- src/blockchain.js
+++ src/blockchain.js
@@ -4,6 +4,22 @@ class Blockchain{
     constructor(){
         this.chain = [Block.genesis]
     }
+
+    get(){
+        return this.chain 
+    }
+    get chainSize(){
+        return this.chain.length 
+    }
+    get latestBlock(){
+        return this.chain[this.chainSize - 1]
+    }
+
+    addBlock(new_block){        
+        new_block.previousHash = this.latestBlock.hash 
+        new_block.hash = new_block.calcHash
+        this.chain.push(new_block)
+    }
 }
 
 module.exports = Blockchain
--- src/coin.js
+++ src/coin.js
@@ -1,5 +1,8 @@
 const Blockchain = require('./blockchain')
+const Block = require('./block')
 
 const coin = new Blockchain()
+
+coin.addBlock(new Block(1, Date.now(), ['no transactions']))
 
 console.log(coin)
